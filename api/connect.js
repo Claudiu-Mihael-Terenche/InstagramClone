6615793eec5dc8f605e8d314d263da51d092de54
CLAUDIU-MIHAEL TERENCHE <135633780+Claudiu-Mihael-Terenche@users.noreply.github.com>
2024-02-21 16:45:11 -0500

    Delete api directory

--- api/connect.js
+++
@@ -1,9 +0,0 @@
-import mysql from "mysql"
-
-export const db = mysql.createConnection({
-  host:"database-2.cfsaq26ccihh.us-east-2.rds.amazonaws.com",
-  user:"admin",
-  password:"6DWN7JVn0ENIVr94QWw0Q5",
-  database:"social",
-  port: "3306"
-})
